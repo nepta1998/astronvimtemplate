9fd9d9edced9a40a4e419a88e09ed26506ac5b6d
neptali <njpm28@gmail.com>
2024-04-08 15:18:23 -0400

    change accept suggestion

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,7 +3,7 @@ return {
   event = 'BufEnter',
   config = function ()
     -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-a>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    vim.keymap.set('i', '<C-y>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
     vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
     vim.keymap.set('i', '<C-f>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
     vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
