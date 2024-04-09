935d03966e82dd3a6cb8306427d6e4d6723c5b06
neptali <njpm28@gmail.com>
2024-04-09 11:54:07 -0400

    fix tabnine in cmp

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,6 +1,6 @@
 return {
   'Exafunction/codeium.vim',
-  event = 'BufEnter',
+  -- event = 'BufEnter',
   config = function ()
     -- Change '<C-g>' here to any keycode you like.
     vim.keymap.set('i', '<C-y>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -2,15 +2,18 @@ return {
   "nvim-cmp",
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    table.insert(opts.sources, 1, {
-      name = "cmp_tabnine",
-      group_index = 1,
-      priority = 100,
-    })
-    table.insert(opts.sources, 1, {
-      name = "codeium",
-      group_index = 1,
-      priority = 100,
-    })
+    -- table.insert(opts.sources, 1, {
+    --   name = "cmp_tabnine",
+    --   group_index = 1,
+    --   priority = 100,
+    -- })
+    -- table.insert(opts.sources, 1, {
+    --   name = "codeium",
+    --   group_index = 1,
+    --   priority = 100,
+    -- })
+    local cmp = require("cmp")
+    -- opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "codeium", group_index = 1 } }))
+    opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp_tabnine", group_index = 1, priority = 100 } }))
   end,
 }
