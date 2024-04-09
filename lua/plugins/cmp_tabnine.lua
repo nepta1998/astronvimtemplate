2d1fe8ec0e377cbc830691d339693d842fb3fcd5
neptali <njpm28@gmail.com>
2024-04-09 11:00:55 -0400

    adding tabnine and adding codium to cmp

---
+++ lua/plugins/cmp_tabnine.lua
@@ -0,0 +1,16 @@
+return {
+  "tzachar/cmp-tabnine",
+  build = {
+    "./install.sh",
+    ":CmpTabnineHub",
+  },
+  dependencies = "hrsh7th/nvim-cmp",
+  opts = {
+    max_lines = 1000,
+    max_num_results = 3,
+    sort = true,
+  },
+  config = function(_, opts)
+    require("cmp_tabnine.config"):setup(opts)
+  end,
+}
---
+++ lua/plugins/nvim_cmp.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-cmp",
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, 1, {
+      name = "cmp_tabnine",
+      group_index = 1,
+      priority = 100,
+    })
+    table.insert(opts.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 100,
+    })
+  end,
+}
