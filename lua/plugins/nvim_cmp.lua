ea1c413fc82e2f86bb979c129b181b11b42b8434
neptali <njpm28@gmail.com>
2024-04-11 13:41:02 -0400

    change formating

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -30,7 +30,7 @@ return {
         formatting = {
             format = require('lspkind').cmp_format({
                 mode = "symbol",
-                maxwidth = 200,
+                maxwidth = 100,
                 ellipsis_char = '...',
                 symbol_map = { Codeium = "🔮", TabNine = "🧙"}
             })
