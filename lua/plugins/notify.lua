7e3035ee1a3db130444fd05c151d8e0931f41fbd
neptali <njpm28@gmail.com>
2024-04-18 00:04:05 -0400

    adding notify fix background

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,8 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  "rcarriga/nvim-notify",
+  -- require telescope and load extensions as necessary
+  opts = {
+    background_colour = "#000000"
+  }
+}
