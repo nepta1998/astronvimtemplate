1f67923170ab7ac4a16c347a7e796319b99aeb94
neptali <njpm28@gmail.com>
2024-04-21 13:36:05 -0400

    adding trouble and surround plugins

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,41 @@
+return {
+  "folke/trouble.nvim",
+  branch = "dev", -- IMPORTANT!
+  keys = {
+    {
+    "<leader>2",
+      name = "Trouble", desc = "👀" .. "Trouble"
+    },
+    {
+      "<leader>2x",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>2X",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>2s",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>2l",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>2L",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>2q",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+}
