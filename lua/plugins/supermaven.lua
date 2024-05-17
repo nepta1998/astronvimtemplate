41fe29f9efc1d87687e322955769aca3894aded4
neptali <njpm28@gmail.com>
2024-05-17 18:35:13 -0400

    adding supermaven

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,19 @@
+return {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          -- accept_suggestion = "<Tab>",
+          -- clear_suggestion = "<C-]>",
+          accept_suggestion = "<A-f>",
+          clear_suggestion = "<A-c>",
+          accept_word = "<C-j>",
+        },
+        ignore_filetypes = { cpp = true },
+        color = {
+          suggestion_color = "#ffffff",
+          cterm = 244,
+        }
+      })
+    end,
+}
