d147473ec1149f6d74557f3ff2f452a0ec341663
neptali <njpm28@gmail.com>
2024-05-20 08:13:34 -0400

    priority supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,7 +1,10 @@
 return {
     "supermaven-inc/supermaven-nvim",
     config = function()
-        require("supermaven-nvim").setup({})
+        require("supermaven-nvim").setup({
+            disable_inline_completion = false, -- disables inline completion for use with cmp
+            disable_keymaps = false -- disables built in keymaps for more manual control
+        })
     end,
     -- config = function()
     --   require("supermaven-nvim").setup({
